e0024c8f0bbb6ee05f7698594d14eb8736aa1903
IVANNNNN <gravemail@yandex.ru>
2022-08-03 22:22:25 +0300

    Example IF

---
+++ example005_If/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(
+    username.ToLower() == "ivan"
+    
+    )
+{
+    Console.WriteLine("Ура, это IVAN!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
