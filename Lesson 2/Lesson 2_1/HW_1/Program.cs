c8d76f01ea3483db146c1b89922b976cb98c7406
IVANNNNN <gravemail@yandex.ru>
2022-08-06 11:22:07 +0300

    Homework 1

---
+++ Lesson 2/Lesson 2_1/HW_1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Программа проверки максимального числа");
+Console.WriteLine("Введите первое целое число ");
+string? numberString = Console.ReadLine ();
+
+int number = int.Parse(numberString!);
+
+Console.WriteLine("Программа проверки");
+Console.WriteLine("Введите второе целое число ");
+string? numberString2 = Console.ReadLine ();
+
+int number2 = int.Parse(numberString2!);
+
+if (number > number2)
+
+{
+    Console.Write ("Max number is " + number);
+}
+
+else
+{
+     Console.Write ("Max number is " + number2);
+}
